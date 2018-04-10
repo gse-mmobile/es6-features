62a60b5783cb155642cde246dba173cc41eaf95a
moses.adebayo <moses.adebayo@venturegardengroup.com>
2018-04-10 15:39:26 +0100

    spread operator for flexible arguments

--- spreadOperator.js
+++ spreadOperator.js
@@ -11,3 +11,18 @@ console.log('5 * 10 * 4:', multiplication(...num));
 const testData = ["Aero", "Arik", "British"];
 const copyTestData = [...testData, "Medview"];
 console.log(copyTestData);
+
+/*
+* Convert string to Array
+* */
+const name = "moscoworld";
+const name_split = [...name];
+console.log(name_split);
+
+/*
+* Functions that accept any number of arguments
+* */
+const numbers = [1, 2, 3, 4, 5, 6];
+const min_number = Math.min(...numbers);
+const max_number = Math.max(...numbers);
+console.log('[Min, Max]', [min_number, max_number]);
